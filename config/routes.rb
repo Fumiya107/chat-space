deb4a8aaa6e4fc615780d3c959bbe694b100fc74
Fumiya107 <bnpbb822@ybb.ne.jp>
2019-04-09 17:37:36 +0900

    Change some2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< Updated upstream
   root 'messages#index'
+=======
+    root 'messages#index'
+>>>>>>> Stashed changes
 end
